864e7a9dc43626b9f82400ac26279fe8a5cf583d
Anna Bakirova <32470916+annabakirova@users.noreply.github.com>
2021-10-17 23:10:37 +0300

    programm to print collatz sequence@

---
+++ CollatzSequence/Source.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+unsigned long long readULL() {
+	std::cout << "Enter a number:";
+	unsigned long long n;
+	std::cin >> n;
+	while (std::cin.fail() || (static_cast<unsigned long long>(n) != n) || n > ULLONG_MAX) {
+		std::cout << "Wrong input, try again:\n";
+		std::cin.clear();
+		std::cin.ignore(256, '\n');
+		std::cin >> n;
+	}
+	return n;
+}
+
+int main() {
+	unsigned long long x;
+	x = readULL();
+	while (x > 1) {
+		if (x & 1) {
+			x += x << 1;
+			x++;
+			std::cout << x << " ";
+		}
+		else {
+			x >>= 1;
+			std::cout << x << " ";
+		}
+	}
+	std::cout << "\n";
+	return 0;
+}
