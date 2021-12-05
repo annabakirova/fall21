ce2d95e1fd1b7185a71f5deb464d457871c10bfc
Anna Bakirova <32470916+annabakirova@users.noreply.github.com>
2021-12-05 23:21:39 +0300

    search for element in linear binary array by index

--- 29.11/Source.cpp
+++ 29.11/Source.cpp
@@ -4,56 +4,58 @@
 
 using namespace std;
 
-bool testCopyFile(string in, string out) {
-	string sample = "12\r\n34\r\n";
-	int size = 2; //8 bytes
+char getElementByIndex(string in, int r, int c) {
 	ifstream inFile;
-	ofstream outFile;
-	inFile.open("1.txt", ios::binary);
-	outFile.open("2.txt", ios::binary);
+	inFile.open(in, ios::binary | ios::in);
+	char num;
+	if (!inFile.is_open()) {
+		cout << "failed to open";
+	}
+	else {
+		int rs = 0;
+		int cs = 0;
+		inFile.read(&num, sizeof(char));
+		while (num != '\r') {
+			rs *= 10;
+			rs += num - '0';
+			inFile.read(&num, sizeof(char));
+		}
+		inFile.seekg(1, ios_base::cur);
+
+		inFile.read(&num, sizeof(char));
+		while (num != '\r') {
+			cs *= 10;
+			cs += (num - '0');
+			inFile.read(&num, sizeof(char));
+		}
+		//cout << rs << " " << cs << endl;
+		inFile.seekg(1, ios_base::cur);
+		//cout << "current position: " << inFile.tellg() << "\n";
+		int pos = cs * (r - 1) + c - 1;
+		inFile.seekg(pos, ios_base::cur);
+		//cout << "current position: " << inFile.tellg() << "\n";
+		inFile.read(&num, sizeof(char));
+		inFile.close();
+		return num;
+	}
 }
 
-int main(int argc, char** argv) {
-	string in;
-	string out;
-	if (argc > 0) {
-		in = argv[1];
-		out = argv[2];
-	}
+void saveEvery4ByteToFile(string in, string out) {
 	ifstream inFile;
 	ofstream outFile;
 	inFile.open(in, ios::binary);
 	outFile.open(out, ios::binary);
 
-	/*string line;
-	if (myFile1.is_open() && myFile.is_open()) {
-		while (getline(myFile1, line)) {
-			myFile << line;
-		}
-		myFile.close();
-		myFile1.close();
+	size_t size = 1;
+	char* buffer = new char[size];
+	if (!inFile.is_open() || !outFile.is_open()) {
+		cout << "failed to open";
 	}
 	else {
-		cout << "failed to open";
-	}*/
-
-
-	size_t size = 1;
-	size_t counter = 0;
-	int r = 1;
-	int c = 0;
-
-	char* buffer = new char[size];
-	if (inFile.is_open() && outFile.is_open()) {
 		while (!inFile.eof()) {
-			//cout << inFile.tellg();
 			inFile.read(buffer, size * sizeof(char));
-			//cout << inFile.gcount() << " ";
-			counter += size * sizeof(char);
 			inFile.seekg(3, ios_base::cur);
-			//cout << inFile.tellg();
 			outFile.write(buffer, size * sizeof(char));
-			
 			if (inFile.tellg() == -1) {
 				break;
 			}
@@ -61,10 +63,14 @@ int main(int argc, char** argv) {
 		outFile.close();
 		inFile.close();
 	}
-	else {
-		cout << "failed to open";
-	}
-
 	delete[] buffer;
+}
 
+int main(int argc, char** argv) {
+	if (argc > 2) {
+		cout << argc << endl;
+		string in = argv[1];
+		cout << in << endl;
+		cout << "element at row " << 10 << " and column 2 is " << getElementByIndex(in, 10, 2) << endl;
+	}
 }
